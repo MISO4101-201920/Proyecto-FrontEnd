56c6b79d43163e032c9c4c60e3e4e1dbcb124fa0
SergioAndes <54184733+SergioAndes@users.noreply.github.com>
2019-10-09 19:20:37 -0500

    Crear marca en componente modal-video

--- src/app/video-modal/video-modal.module.ts
+++ src/app/video-modal/video-modal.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {NgbdModalContent, VideoModalComponent} from './video-modal.component';
+import {NgbdModal1Content, NgbdModalContent, VideoModalComponent} from './video-modal.component';
 import { Routes, RouterModule } from '@angular/router';
 import { YoutubePlayerModule } from 'ng2-youtube-player';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { HttpModule } from '@angular/http';
+import {FormsModule} from "@angular/forms";
 
 const routes: Routes = [
   { path: '', component: VideoModalComponent },
 ];
 
 @NgModule({
-  declarations: [VideoModalComponent, NgbdModalContent],
+  declarations: [VideoModalComponent, NgbdModalContent, NgbdModal1Content],
   imports: [
     NgbModule,
     YoutubePlayerModule,
     CommonModule,
-    RouterModule.forChild(routes)
+    HttpModule,
+    RouterModule.forChild(routes),
+    FormsModule
   ],
   exports: [RouterModule, VideoModalComponent],
   entryComponents: [NgbdModalContent]
