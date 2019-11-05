0601d773d5236511d1189ef64ae98945704c1af8
Jhonnyguzz <jhjguzmanri@unal.edu.co>
2019-11-05 14:21:25 -0500

    Adding view and service for detail interactive content

--- src/app/services/contenido.service.ts
+++ src/app/services/contenido.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 
@@ -11,7 +11,7 @@ export class ContenidoService {
 
   private contenidoUrl = `${environment.apiUrl}/content/interactive_content/`;
   private reportesUrl = `${environment.apiUrl}/activities/reports/`;
-
+  private detalleUrl = `${environment.apiUrl}/content/interactivecontent/`;
 
   constructor(private httpClient: HttpClient) { }
 
@@ -26,6 +26,43 @@ export class ContenidoService {
     };
     console.log('bodyy', body);
     return this.httpClient.post(this.contenidoUrl, body);
+  }
+
+  getDetalleContenidoInteractivo(contentId: number): Observable<any> {
+    return this.httpClient.get<any>(this.detalleUrl + contentId);
+    /*const dataArr = {
+      nombre: 'Mi Contenido Interactivo 1',
+      tieneRetroalimentacion: true,
+      calificacion: 5,
+      contenido: {
+        nombre: "Video 1",
+        url: "youtube.com",
+        fuente: "Youtube"
+      },
+      cursos: [
+        {
+          nombre: "Curso 1",
+          descripcion: "Este es el curso numero 1"
+        },
+        {
+          nombre: "Curso 2",
+          descripcion: "Este es el curso numero 2"
+        }
+      ],
+      marcas: [
+        {
+          id: 1,
+          punto: 153
+        },
+        {
+          id: 2,
+          punto: 2253
+        }
+      ]
+    }
+
+    //return of(dataArr);*/
+
   }
 
   getReporteContenido(contentId: number): Observable<any>  {
