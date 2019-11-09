bf1d5dfe13957e7d4ec71bec553f74554b89c4bb
Jhonnyguzz <jhjguzmanri@unal.edu.co>
2019-11-09 00:16:39 -0500

    Fixing report due broken model

--- src/app/services/contenido.service.ts
+++ src/app/services/contenido.service.ts
@@ -40,87 +40,9 @@ export class ContenidoService {
 
   getDetalleContenidoInteractivo(contentId: number): Observable<any> {
     return this.httpClient.get<any>(this.detalleUrl + contentId);
-    /*const dataArr = {
-      nombre: 'Mi Contenido Interactivo 1',
-      tieneRetroalimentacion: true,
-      calificacion: 5,
-      contenido: {
-        nombre: "Video 1",
-        url: "youtube.com",
-        fuente: "Youtube"
-      },
-      cursos: [
-        {
-          nombre: "Curso 1",
-          descripcion: "Este es el curso numero 1"
-        },
-        {
-          nombre: "Curso 2",
-          descripcion: "Este es el curso numero 2"
-        }
-      ],
-      marcas: [
-        {
-          id: 1,
-          punto: 153
-        },
-        {
-          id: 2,
-          punto: 2253
-        }
-      ]
-    }
-
-    //return of(dataArr);*/
-
   }
 
   getReporteContenido(contentId: number): Observable<any>  {
-  return this.httpClient.get<any>(this.reportesUrl + contentId);
-    // const dataTest = {
-    //   nombre: 'profesor1',
-    //   marcas: [{
-    //     nombre: 'marca1',
-    //     actividades: [{
-    //       nombre: 'actividad1',
-    //       tipo: 'multiple',
-    //       opciones: [
-    //         {
-    //           respuesta: 'posible respuesta 1',
-    //           esCorrecta: true,
-    //           votos: 5
-    //         },
-    //         {
-    //           respuesta: 'posible respuesta 2',
-    //           esCorrecta: false,
-    //           votos: 5
-    //         },
-    //         {
-    //           respuesta: 'posible respuesta 3',
-    //           esCorrecta: false,
-    //           votos: 5
-    //         }
-    //       ]
-    //     },
-    //     {
-    //       nombre: 'actividad2',
-    //       tipo: 'verdadero/falso',
-    //       opciones: [
-    //         {
-    //           respuesta: 'posible respuesta 1',
-    //           esCorrecta: true,
-    //           votos: 5
-    //         },
-    //         {
-    //           respuesta: 'posible respuesta 2',
-    //           esCorrecta: false,
-    //           votos: 5
-    //         },
-    //       ]
-    //     }
-    //     ]
-    //   }]
-    // };
-    // return dataTest;
+    return this.httpClient.get<any>(this.reportesUrl + contentId);
   }
 }
