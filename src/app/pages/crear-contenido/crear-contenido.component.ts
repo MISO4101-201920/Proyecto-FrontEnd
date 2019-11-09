5a7d0e298f221a4e76799983ccce63ef1d5eea97
Maria Arevalo <mariaisabelarevalo@hotmail.com>
2019-11-09 00:29:23 -0500

    redireccion configurar

--- src/app/pages/crear-contenido/crear-contenido.component.ts
+++ src/app/pages/crear-contenido/crear-contenido.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { LoadVideoService } from '../../services/contenidoInter/load-video.service';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ContenidoService } from '../../services/contenido.service'
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-crear-contenido',
@@ -55,14 +56,18 @@ export class ModalAsociarContenidoInt {
   constructor(
     public dialogRef: MatDialogRef<ModalAsociarContenidoInt>,
     @Inject(MAT_DIALOG_DATA) public data,
-    public _contenidoService: ContenidoService) { }
+    public _contenidoService: ContenidoService,
+    public router: Router) { }
 
   onNoClick(): void {
     this.dialogRef.close();
   }
 
   crearContenido(nombre, id) {
-    this._contenidoService.postContenidoInteractivo(nombre.value, id);
+    this._contenidoService.postContenidoInteractivo(nombre.value, id).subscribe(result => {
+      console.log('resulttt',result);
+      this.router.navigate(['contenido-interactivo/configurar/', result['id']]);
+    });
   }
 
 }
